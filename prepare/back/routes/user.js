a6cf01946ce61818317f9f770c7bf81d491933d8
iseoryu <rks85227@gmail.com>
2024-05-17 17:37:01 +0900

    login

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+
 const { User } = require("../models");
 const router = express.Router();
+
+router.post("/login", (req, res, next) => {
+  //미들웨어 확장
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.error(err);
+      next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+  })(req, res, next);
+});
 
 router.post("/", async (req, res, next) => {
   try {
